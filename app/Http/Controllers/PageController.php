c9f4e3b1b26c153d313c192d665eed924811b669
Nizar Khawarizmi <nizar.banme@gmail.com>
2024-03-02 16:22:21 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,10 @@
+<?php
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+class PageControllers extends Controller
+{
+    public function hello(){
+        return 'Hello World';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Basic Routing
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+Route::get('/world', function () {
+    return 'World';
+});
+Route::get('/', function () {
+    return 'Selamat Datang';
+});
+Route::get('/about', function () {
+    return '2341728020, Nizar Khawarizmi';
+});
+
+// Route Parameters
+Route::get('/user/{name}', function($name){
+    return 'Nama saya '.$name;
+});
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId){
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function($id){
+    return 'Halaman artikel dengan ID '.$id;
+});
+
+/* Optional Parameters */
+Route::get('/user/{name?}', function($name='John'){
+    return 'Nama saya '.$name;
+});
+
+/* Menambahkan controller "WelcomeController" pada route */
+Route::get('/hello', [WelcomeController::class,'hello']);
